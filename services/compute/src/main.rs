66b252d6d8d63dbcfb75fd21557b011950729d89
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2025-11-04 15:30:48 +0500

    changed hardcoded server addresses to compute and billing to resolve alread in use errors

--- services/compute/src/main.rs
+++ services/compute/src/main.rs
@@ -136,9 +136,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .with_state(app_state);
 
     info!("Server running on port {:#?}", config.server_addres);
-    let listener = tokio::net::TcpListener::bind(config.clone().server_addres.clone())
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8002").await.unwrap();
+    // let listener = tokio::net::TcpListener::bind(config.clone().server_addres.clone())
+    //     .await
+    //     .unwrap();
     axum::serve(
         listener,
         app.into_make_service_with_connect_info::<SocketAddr>(),
