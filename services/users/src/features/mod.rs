d45ae30dc27647d4a8b2fee7aedfff22f353123d
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2025-11-12 15:55:42 +0500

    fixed some sqlx queries and repository getter names

--- services/users/src/features/mod.rs
+++ services/users/src/features/mod.rs
@@ -8,28 +8,30 @@ use crate::utilities::app_state::AppState;
 
 use axum::{
     Router,
-    routing::{delete, get, patch, post},
+    routing::{get, post},
 };
 
 pub fn routes() -> Router<AppState> {
     Router::new()
-        .route("/api/v1/profile", get(handlers::get_user_handler))
-        .route("/api/v1/profile", patch(handlers::update_user_handler))
-        .route("/api/v1/profile", delete(handlers::delete_user_handler))
-        .route("/api/v1/logout", get(handlers::logout_handler))
+        .route(
+            "/api/v1/profile",
+            get(handlers::get_user_handler)
+                .patch(handlers::update_user_handler)
+                .delete(handlers::delete_user_handler),
+        )
         .route("/api/v1/auth/refresh", post(handlers::refresh_handler))
         .route("/api/v1/auth/logout", get(handlers::logout_handler))
+        .route("/api/v1/auth/verify", get(handlers::verify_handler))
+        .route("/api/v1/auth/google", get(handlers::google_oauth_handler))
+        .route("/api/v1/auth/github", get(handlers::github_oauth_handler))
         .route(
             "/api/v1/auth/email",
             post(handlers::continue_with_email_handler),
         )
-        .route("/api/v1/auth/verify", get(handlers::verify_handler))
-        .route("/api/v1/auth/google", get(handlers::google_oauth_handler))
         .route(
             "/api/v1/auth/google/callback",
             get(handlers::google_oauth_callback_handler),
         )
-        .route("/api/v1/auth/github", get(handlers::github_oauth_handler))
         .route(
             "/api/v1/auth/github/callback",
             get(handlers::github_oauth_callback_handler),
